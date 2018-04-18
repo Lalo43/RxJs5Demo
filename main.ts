d620c4126e1b87c511708c6e4e18e588552ec9dd
JAVA8 <deophis@gmail.com>
2018-04-18 15:26:00 -0500

    actualizado

--- main.ts
+++ main.ts
@@ -1,34 +1,54 @@
 import { Observable, Observer } from "rxjs";
+import { ArrayObservable } from "rxjs/observable/ArrayObservable";
+import { ForkJoinObservable } from "rxjs/observable/ForkJoinObservable";
 
 let output = document.getElementById('output');
+let input = document.getElementById('input');
 let button = document.getElementById('button');
 let click = Observable.fromEvent(button,'click');
+
+let button1 = document.getElementById('button1');
+let click1 = Observable.fromEvent(button,'click1');
+let output1 = document.getElementById('output1');
 /*source.subscribe(new MyObservable);*/
 
 function load(url: string){
+    return Observable.create(observer =>{
     let xhr = new XMLHttpRequest();
 
-    xhr.addEventListener('load',() =>{
-        let jsonStarWars = JSON.parse(xhr.responseText);
-        jsonStarWars.forEach(element => {
-            let div = document.createElement('div');
-            div.innerText = element.name;
-            output.appendChild(div);            
-        });
-    });
+        xhr.addEventListener('load',() =>{
 
-    xhr.open('GET', url)
-    xhr.send();
+            if(xhr.status === 200){
+                let jsonEjercicio = JSON.parse(xhr.responseText);
+                observer.next(jsonEjercicio);
+                observer.complete();
+            }else{
+                observer.error(xhr.statusText);
+            }        
+                  
+        }); 
+
+        xhr.open('GET', url)
+        xhr.send();
+
+    });    
 }
 
-click.subscribe(
-    value => {
-       load('starwars.json');
-    },
-    error => {
-        console.log(`Error: ${error}`);
-    },
-    () => {
-        console.log('Complete');
-    }
-);
+function renderEjercicio(jsonEjercicio){
+    jsonEjercicio.forEach(element => {
+            let div = document.createElement('div');
+            div.innerText = `${element.name}`;
+            output.appendChild(div);  
+    }); 
+}
+
+
+click.flatMap(x => load('ejercicioobs.json'))
+     .subscribe((x: any) => Observable.from(x).filter((x: any) => x.calif >=60).subscribe(
+         x =>console.log(x.name)));
+
+
+click.flatMap(x => load('ejercicioobs.json'))
+.subscribe((x: any) => Observable.from(x).max((x: any) => x.calif).subscribe(
+    x =>console.log(x.calif)));
+
